7d4694d04155085a3b54324d34fb21160d74340a
Your Name <your@email.com>
2020-11-05 03:00:37 +0000

    add move up

--- client.js
+++ client.js
@@ -13,6 +13,30 @@ const connect = function () {
   });
   conn.setEncoding("utf8");
   conn.on("connect", () => {
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 50);
+    setTimeout(() => {
+      conn.write("Move: down");
+    }, 100);
+    setTimeout(() => {
+      conn.write("Move: left");
+    }, 150);
+    setTimeout(() => {
+      conn.write("Move: right");
+    }, 200);
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 250);
+    setTimeout(() => {
+      conn.write("Move: down");
+    }, 300);
+    setTimeout(() => {
+      conn.write("Move: left");
+    }, 350);
+    setTimeout(() => {
+      conn.write("Move: right");
+    }, 400);
     conn.write("Name: SNK");
     console.log("Successfully connected to game server");
   });
